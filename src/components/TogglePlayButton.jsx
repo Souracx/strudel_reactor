4f1d11546b39101612f8ef974df607a80bab409a
Souradeep Chakma <souradeep.chakma@gmail.com>
2025-11-20 20:48:16 +1030

    Update and fix

--- src/components/TogglePlayButton.jsx
+++ src/components/TogglePlayButton.jsx
@@ -1,22 +1,34 @@
+function TogglePlayButton({globalEditor, songText, isPlaying, setIsPlaying, showNotification}) { 
 
-function TogglePlayButton({onPlay, onStop, isPlaying}) { 
-   
+    const handlePlay = () => {
+
+        if (!globalEditor) return showNotification('Editor not ready yet. Please wait...', 'warning');
+        //check for empty code 
+        if(!songText) return showNotification('Please add some code first','error'); 
+        
+        globalEditor.evaluate()
+        setIsPlaying(true); 
+        showNotification('Playback started!', 'success');
+    }
+
+    const handleStop = () => {
+        globalEditor.stop() 
+        setIsPlaying(false); 
+        showNotification('Paused','success')
+    }
 
     const handleToggle = () => {
         if (isPlaying) {
-            onStop();
-            
+            handleStop();
         } else {
-            onPlay();
-            
+            handlePlay();
         }
     };
 
     return( 
         <button 
             className={`toggle-play-btn ${isPlaying ? 'playing' : 'stopped'}`}
-            onClick={handleToggle}
-        >
+            onClick={handleToggle}>
             {isPlaying ? '■ stop' : '▶ play'}
         </button>
     ); 
